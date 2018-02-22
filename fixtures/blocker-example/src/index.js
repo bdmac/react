46d82ce456741d59133df6e685e24fd1806f6992
Dan Abramov <dan.abramov@gmail.com>
2018-02-22 02:17:19 +0000

    omg omg

--- fixtures/blocker-example/src/index.js
+++ fixtures/blocker-example/src/index.js
@@ -10,26 +10,66 @@ class MarkdownRenderer extends React.PureComponent {
   }
 }
 
+class AsyncValue extends React.Component {
+  state = {asyncValue: this.props.defaultValue};
+  componentDidMount() {
+    ReactDOM.unstable_deferredUpdates(() => {
+      this.setState((state, props) => ({asyncValue: props.value}));
+    });
+  }
+  componentDidUpdate() {
+    if (this.props.value !== this.state.asyncValue) {
+      ReactDOM.unstable_deferredUpdates(() => {
+        this.setState((state, props) => ({asyncValue: props.value}));
+      });
+    }
+  }
+  render() {
+    return this.props.children(this.state.asyncValue);
+  }
+}
+
 class App extends React.Component {
   state = {
     input: DEFAULT_INPUT,
-    result: DEFAULT_INPUT
+    // result: DEFAULT_INPUT
   };
   handleChange = (e) => {
     const input = e.target.value;
-    Promise.resolve().then(() => {
-      ReactDOM.unstable_deferredUpdates(() => {
-        this.setState({
-          result: input
-        });
-      })      
-    });
+    // Promise.resolve().then(() => {
+    //   ReactDOM.unstable_deferredUpdates(() => {
+    //     this.setState(state => ({
+    //       result: state.input
+    //     }));
+    //   })      
+    // });
 
     this.setState({
       input
     });
   };
   render() {
+
+    // console.log("Input:")
+    // console.log(this.state.input.substr(
+    //   this.state.input.indexOf('##'),
+    //   this.state.input.indexOf('- [') - this.state.input.indexOf('##')
+    // ))
+
+    // console.log("Result:")
+    // console.log(this.state.result.substr(
+    //   this.state.result.indexOf('##'),
+    //   this.state.result.indexOf('- [') - this.state.result.indexOf('##')
+    // ))
+    // console.log('-----')
+
+    // h
+    // hel
+    // hello w
+    // hello world
+    // hello w
+    // hello world
+
     return (
       <React.Fragment>
         <div style={{float: 'left', width: '50%', height: '100%', padding: 40}}>
@@ -38,7 +78,11 @@ class App extends React.Component {
         </div>
         <div style={{float: 'left', width: '50%', height: '100%', padding: 40}}>
           <h1>Preview</h1>
-          <MarkdownRenderer source={this.state.result} />
+
+          <AsyncValue defaultValue="" value={this.state.input}>
+            {asyncInput => <MarkdownRenderer source={asyncInput} />}
+          </AsyncValue>
+
         </div>
       </React.Fragment>
     );
@@ -47,4 +91,9 @@ class App extends React.Component {
 
 
 const container = document.getElementById('root');
-ReactDOM.render(<App />, container);
+ReactDOM.render(
+  <React.unstable_AsyncMode>
+    <App />
+  </React.unstable_AsyncMode>,
+  container
+);

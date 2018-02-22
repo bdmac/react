f215d0ae03638dc4d32e42eb26277efea15cc15e
Dan Abramov <dan.abramov@gmail.com>
2018-02-22 15:33:52 +0000

    okay

--- fixtures/blocker-example/src/index.js
+++ fixtures/blocker-example/src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Markdown from 'react-markdown';
-import DEFAULT_INPUT from './README';
+import _ from 'lodash';
+import {
+  VictoryChart,
+  VictoryAxis,
+  VictoryArea,
+  VictoryLine,
+  Area
+} from 'victory';
 import './index.css';
-
-class MarkdownRenderer extends React.PureComponent {
-  render() {
-    return <Markdown {...this.props} />
-  }
-}
 
 class SyncValue extends React.Component {
   render() {
@@ -16,46 +16,8 @@ class SyncValue extends React.Component {
   }
 }
 
-function debounce(func, wait, immediate) {
-  var timeout;
-  return function() {
-    var context = this, args = arguments;
-    var later = function() {
-      timeout = null;
-      if (!immediate) func.apply(context, args);
-    };
-    var callNow = immediate && !timeout;
-    clearTimeout(timeout);
-    timeout = setTimeout(later, wait);
-    if (callNow) func.apply(context, args);
-  };
-};
-
-class DebouncedValue extends React.PureComponent {
-  state = {debouncedValue: this.props.defaultValue};
-
-  update = debounce(() => {
-    ReactDOM.flushSync(() => {
-      this.setState({
-        debouncedValue: this.props.value
-      });      
-    });
-  }, 500);
-
-  componentDidUpdate() {
-    if (this.props.value !== this.state.debouncedValue) {
-      this.update();
-    }
-  }  
-
-  render() {
-    return this.props.children(this.state.debouncedValue);
-  }
-}
-
-
 class AsyncValue extends React.PureComponent {
-  state = {asyncValue: this.props.defaultValue};
+  state = {asyncValue: this.props.value};
   componentDidMount() {
     ReactDOM.unstable_deferredUpdates(() => {
       this.setState((state, props) => ({asyncValue: props.value}));
@@ -73,57 +35,177 @@ class AsyncValue extends React.PureComponent {
   }
 }
 
-const types = {
-  sync: SyncValue,
-  async: AsyncValue,
-  debounced: DebouncedValue
+class Demo extends React.Component {
+  state = {
+    async: false,
+  };
+
+  render() {
+    const Strategy = this.state.async ? AsyncValue : SyncValue;
+    return (
+      <div>
+        <label>
+          <input type="checkbox" checked={this.state.async} onChange={(e) => this.setState({ async: e.target.checked })} />
+          Async
+        </label>
+        <Strategy value={this.props.value}>{this.props.children}</Strategy>
+      </div>
+    )
+  }
 }
 
-class App extends React.Component {
-  state = {
-    input: DEFAULT_INPUT,
-    strategy: 'sync'
-  };
-  handleChange = (e) => {
-    const input = e.target.value;
-    this.setState({
-      input
-    });
-  };
+// This custom component is supplied in place of Path
+class GradientPath extends React.Component {
+  toGrayscale(color) {
+    const integerColor = parseInt(color.replace("#", ""), 16);
+    const r = (integerColor >> 16) & 255;
+    const g = (integerColor >> 8) & 255;
+    const b = integerColor & 255;
+    const gray = parseInt(0.299 * r + 0.587 * g + 0.114 * b, 10);
+    return `rgb(${gray}, ${gray}, ${gray})`;
+  }
   render() {
-    const strategy = this.state.strategy;
-    const Strategy = types[strategy];
+    const { style, d, events } = this.props;
+    const gradientId = `gradient-${Math.random()}`;
+    const areaStyle = Object.assign(
+      {}, style, { fill: `url(${window.location.href}#${gradientId})` }
+    );
+    const percent = `${this.props.percent}%`;
+    const gray = this.toGrayscale(style.fill);
     return (
-      <React.Fragment>
-        <div style={{float: 'left', width: '50%', height: '100%', padding: 40}}>
-          <h1>Live Markdown Editor</h1>
-          <select value={this.state.strategy} onChange={e => this.setState({ strategy: e.target.value })}>
-            <option value='sync'>sync</option>
-            <option value='debounced'>debounced</option>
-            <option value='async'>async</option>
-          </select>
-          <textarea style={{ width: '100%', height: '100%' }} value={this.state.input} onChange={this.handleChange} />
-        </div>
-        <div style={{float: 'left', width: '50%', height: '100%', padding: 40}}>
-          <h1>Preview</h1>
-
-          <div>
-            <Strategy defaultValue={this.state.input} value={this.state.input}>
-              {asyncInput => (
-                <div style={{ opacity: asyncInput === this.state.input ? 1 : 0.8 }}>
-                  <MarkdownRenderer source={asyncInput} />
-                </div>
-              )}
-            </Strategy>
-          </div>
-
-        </div>
-      </React.Fragment>
+      <g key="area">
+        <defs>
+          <linearGradient id={gradientId}>
+              <stop offset="0%" stopColor={style.fill}/>
+              <stop offset={percent} stopColor={style.fill}/>
+              <stop offset={percent} stopColor={gray}/>
+              <stop offset="100%" stopColor={gray}/>
+          </linearGradient>
+        </defs>
+        <path key="area" style={areaStyle} d={d} {...events}/>
+      </g>
     );
   }
 }
 
+class VictoryExample extends React.PureComponent {
+  render() {
+    const streamData = this.props.streamData;
+    const colors = [
+      "#006064", "#00796B", "#8BC34A", "#DCE775",
+      "#FFF59D", "#F4511E", "#c33409"
+    ];
 
+    return (
+      <VictoryChart
+        width={400} height={400}
+        domain={{ x: [0, 25], y: [-250, 250] }}
+      >
+        <VictoryAxis
+          style={{
+            axis: { stroke: "none" },
+            ticks: { stroke: "none" },
+            tickLabels: { fill: "none" },
+            grid: { stroke: "lightGray" }
+          }}
+          tickCount={20}
+        />
+        <VictoryAxis dependentAxis
+          style={{
+            ticks: { stroke: "gray" },
+            tickLabels: { fill: "gray", fontSize: 12 }
+          }}
+          crossAxis={false}
+        />
+
+        {
+          streamData.map((d, i) => {
+            return (
+              <VictoryArea key={i}
+                interpolation="monotoneX"
+                data={d}
+                style={{ data: { fill: colors[i], stroke: "none" } }}
+                dataComponent={
+                  <Area
+                    pathComponent={<GradientPath percent={this.props.percent}/>}
+                  />
+                }
+              />
+            );
+          })
+        }
+        <VictoryLine
+          style={{
+            data: { stroke: "#c33409", strokeWidth: 3 }
+          }}
+          data={[
+            { x: 25 * this.props.percent / 100, y: -300 },
+            { x: 25 * this.props.percent / 100, y: 300 }
+          ]}
+        />
+      </VictoryChart>
+    );
+  }
+}
+
+let cachedData = new Map();
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      complexity: 30
+    };
+  }
+  // This data is manipulated to approximate a stream.
+  getStreamData(input) {
+    if (cachedData.has(input)) {
+      return cachedData.get(input);
+    }
+    const data = _.range(7).map((i) => {
+      return _.range(this.state.complexity).map((j) => {
+        return {
+          x: j,
+          y: (10 - i) * _.random(10 - i, 20 - 2 * i),
+          _y0: -1 * (10 - i) * _.random(10 - i, 20 - 2 * i)
+        };
+      });
+    });
+    cachedData.set(input, data);
+    return data;
+  }
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.complexity !== this.state.complexity) {
+      cachedData.clear();
+    }
+  }
+  render() {
+    return (
+      <div  style={{ width: '100vw', float: 'left' }}>
+        <div style={{ width: '40vw', float: 'left' }}>
+          <input style={{ fontSize: '30px' }} value={this.state.input} onChange={e => this.setState({ input: e.target.value })} />
+          <br />
+          Chart complexity: <input type="range" min="30" max="2000" value={this.state.complexity} onChange={e => this.setState({ complexity: e.target.value })} />
+          <br />
+          <h3>You typed {this.state.input}</h3>
+          <br />
+          <img src="https://media0.giphy.com/media/nNxT5qXR02FOM/giphy.gif" />
+        </div>
+        <div style={{ width: '40vw', float: 'left' }}>
+          <Demo value={this.state.input}>
+            {asyncInput =>
+              <VictoryExample
+                percent={100}
+                streamData={this.getStreamData(asyncInput)}
+              />
+            }
+          </Demo>
+        </div>
+      </div>
+    );
+  }
+}
 const container = document.getElementById('root');
 ReactDOM.render(
   <React.unstable_AsyncMode>
